27730b1b658dddd954019ac8d8c57cb172f2590d
chinba <ganzorig_chinbat@unimedia.co.jp>
2019-03-26 15:37:56 +0800

    declare 2d array with malloc

---
+++ math/Matrix_declare_2darray_with_malloc.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Mat {
+    public :
+        int **a;
+        int *n, *m;
+        
+        Mat (int n, int m) {
+            this -> n = (int*) malloc(sizeof(int));
+            this -> m = (int*) malloc(sizeof(int));
+            *(this -> n) = n;
+            *(this -> m) = m;
+            a = (int**) malloc(sizeof(int*) * n);
+            for (int i = 0; i < n; i ++) {
+                a[i] = (int*) malloc(sizeof(int) * m);
+            }
+        }
+        
+        ~Mat() {
+            for (int i = 0; i < *n; i ++) {
+                free (a[i]);
+            } free(a);
+            free(n);
+            free(m);
+        }
+};
+
+
+signed main() {
+    
+    Mat * mat = new Mat(10, 4);
+    
+    cout << *(mat -> n) << endl;
+    cout << *(mat -> m) << endl;
+    int cnt = 0;
+    for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < 4; j++) {
+            mat -> a[i][j] = ++cnt;
+            cout << mat -> a[i][j] << " ";
+        } cout << endl;
+    }
+    
+    delete mat;
+    
+    cout << "after delete :: " << endl;
+    cout << *(mat -> n) << endl;
+    cout << *(mat -> m) << endl;
+    for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < 4; j ++) {
+            cout << mat -> a[i][j] << " ";
+        } cout << endl;
+    }
+    
+    return 0;
+}
